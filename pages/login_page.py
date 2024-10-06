5aed1b125b04a12bbe26d19780780e65b7567390
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-10-06 19:23:59 +0300

    Добавил тесты логина

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,21 +6,23 @@ using Selenium WebDriver.
 """
 
 import logging
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
 from base.base_page import BasePage
-from test_data import login_creds
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class LoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.home_page_login_btn = (By.ID, "login2")
+        self.home_page_signup_btn = (By.ID, "signin2")
         self.home_page_profile_btn = (By.ID, "nameofuser")
         self.home_page_logout_btn = (By.ID, "logout2")
         self.login_page_username_input = (By.ID, "loginusername")
         self.login_page_password_input = (By.ID, "loginpassword")
         self.login_page_login_btn = (By.XPATH, "//button[contains(text(),'Log in')]")
-        self.login_page_close_btn = (By.LINK_TEXT, "Close")
+        self.login_page_close_btn = (By.CSS_SELECTOR, "#logInModal .modal-footer button.btn.btn-secondary")
 
     def open_login_window(self):
         logging.info("Wait for element to be present and click login button")
@@ -43,11 +45,6 @@ class LoginPage(BasePage):
         self.wait_for_element_to_be_present(self.login_page_login_btn)
         self.click_element(self.login_page_login_btn)
 
-    def close_login_window(self):
-        logging.info("Wait for element to be present and click close button")
-        self.wait_for_element_to_be_present(self.login_page_close_btn)
-        self.click_element(self.login_page_close_btn)
-
     def check_login_complete(self, username):
         """ Checks if the login is complete. """
 
@@ -62,6 +59,40 @@ class LoginPage(BasePage):
         logging.warning("Login check failed")
         return False
 
+    def close_login_window(self):
+        logging.info("Wait for element to be present and click close button")
+        self.wait_for_element_to_be_present(self.login_page_close_btn)
+        self.click_element(self.login_page_close_btn)
+
+    def check_close_login_window(self):
+        logging.info("Wait for element to be present and check close login window")
+
+        if self.is_element_invisible(self.login_page_close_btn):
+            logging.info("Login window is closed or not present.")
+            return True
+        else:
+            logging.warning("Login window is still open.")
+            return False
+
     def logout(self):
         logging.info("Wait for element to be present and click logout button")
-        self.click_element(self.home_page_profile_btn)
+
+        try:
+            self.wait_for_element_to_be_present(self.home_page_logout_btn)
+            self.wait.until(EC.visibility_of_element_located(self.home_page_logout_btn))
+            self.click_element(self.home_page_logout_btn)
+        except TimeoutException:
+            logging.error("Timeout while waiting for logout button to be clickable.")
+            raise
+        except StaleElementReferenceException:
+            logging.warning("StaleElementReferenceException caught. Retrying logout.")
+            self.wait_for_element_to_be_present(self.home_page_logout_btn)
+            self.click_element(self.home_page_logout_btn)
+
+    def check_logout_complete(self):
+        if self.is_element_present(self.home_page_login_btn):
+            logging.info("Logout check passed")
+            return True
+        else:
+            logging.warning("Logout check failed")
+            return False
--- tests/test_login.py
+++ tests/test_login.py
@@ -9,13 +9,14 @@ from test_data import login_creds
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.smoke
 @pytest.mark.usefixtures("setup")
 class TestLoginPage:
+    @pytest.mark.smoke
     @allure.title("Login page: login complete")
     @allure.description("Check login complete with correct credentials")
     def test_login_complete(self):
         with allure.step("Opening login page"):
+            logger.info("Open login page")
             login_page = LoginPage(self.driver)
             logger.info("Open login page")
             login_page.open()
@@ -30,9 +31,57 @@ class TestLoginPage:
             logger.info("Click login button")
             login_page.login()
 
-        with ((allure.step("Check login"))):
+        with allure.step("Verify login is complete"):
             logger.info("Check login complete")
             assert login_page.check_login_complete(username=login_creds.STANDARD_USERNAME
                                                    ), "Login is not complete"
-            logger.info("Test login complete passed")
-        
+
+            logger.info("Test Login complete passed")
+
+    @allure.title("Login page: close login window")
+    @allure.description("Check if login window is closed")
+    def test_close_login_window(self):
+        with allure.step("Opening login page"):
+            login_page = LoginPage(self.driver)
+            logger.info("Open login page")
+            login_page.open()
+            login_page.open_login_window()
+
+        with allure.step("Close login window"):
+            logger.info("Click Close button in login window")
+            login_page.close_login_window()
+
+        with allure.step("Verify login window closed"):
+            assert login_page.check_close_login_window(), "Login window is not closed"
+
+            logger.info("Test login window closed passed")
+
+    @pytest.mark.smoke
+    @pytest.mark.xfail(reason="Element not found on DOM")
+    @allure.title("Logout functionality")
+    @allure.description("Check logout functionality after successful login")
+    def test_logout(self):
+        with allure.step("Opening login page"):
+            logger.info("Open login page")
+            login_page = LoginPage(self.driver)
+            login_page.open()
+            login_page.open_login_window()
+
+        with allure.step("Set username and password"):
+            logger.info("Set username and password")
+            login_page.set_users_creds_input(username=login_creds.STANDARD_USERNAME,
+                                             password=login_creds.STANDARD_PASSWORD)
+
+        with allure.step("Login"):
+            logger.info("Click login button")
+            login_page.login()
+
+        with allure.step("Performing logout"):
+            logger.info("Click logout button")
+            login_page.logout()
+
+        with allure.step("Verify logout is successful"):
+            logger.info("Check logout complete")
+            assert login_page.check_logout_complete(), "Logout was not successful"
+
+            logger.info("Test Logout passed")
